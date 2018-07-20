6bfd3fbe09a52d3990cf826aba15078f6b6043e7
jiang <260940356@qq.com>
2018-07-21 01:59:52 +0800

    from pc

---
+++ appstudio/src/main/java/com/example/appstudio/ui/base/BaseDeleteFragment.java
@@ -0,0 +1,10 @@
+package com.example.appstudio.ui.base;
+
+import android.view.View;
+
+public class BaseDeleteFragment extends BaseFragment{
+    @Override
+    protected View onCreateView() {
+        return null;
+    }
+}
