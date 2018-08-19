44ffc7ba6ada968ea6bd3d0c42871ad7b1681832
jiang <260940356@qq.com>
2018-08-20 01:26:42 +0800

    commit with pc(play activity)

--- appstudio/src/main/java/com/sharevar/appstudio/common/serializable/XmlUtil.java
+++ appstudio/src/main/java/com/sharevar/appstudio/common/serializable/XmlUtil.java
@@ -9,9 +9,11 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class XmlUtil {
     public static <T> List<T> listFrom(InputStreamReader reader, Class... beans) {
@@ -20,6 +22,8 @@ public class XmlUtil {
         try {
             parser.setInput(reader);
             int event = parser.getEventType();
+            Stack<Object> stack = new Stack<>();
+            List childList = null;
             while (event != XmlPullParser.END_DOCUMENT) {
                 switch (event) {
                     case XmlPullParser.START_TAG:
@@ -27,18 +31,45 @@ public class XmlUtil {
                         Class bean = CollectionOP.findByAttr(beans, "simpleName", name);
                         if (bean != null) {
                             Object object = bean.newInstance();
+                            if (bean.equals(beans[0])) {
+                                list.add(bean);
+                            } else {
+                                childList = getList(stack.peek());
+                                childList.add(bean);
+                            }
                             int count = parser.getAttributeCount();
                             for (int i = 0; i < count; i++) {
                                 String attrName = parser.getAttributeName(i);
-                                Method setMethod=ObjectTool.setMethod(bean,attrName);
-                                setMethod.invoke(object,parser.getAttributeValue(i));
+                                Method setMethod = ObjectTool.setMethod(bean, attrName);
+                                setMethod.invoke(object, parser.getAttributeValue(i));
                             }
+                            stack.push(object);
                         }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        if (stack.peek().getClass().getSimpleName().equals(parser.getName())) {
+                            stack.pop();
+                        }
+                        break;
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return list;
+    }
+
+    public static List getList(Object object) {
+        Method[] methods = object.getClass().getMethods();
+        for (Method method : methods) {
+            if (method.getReturnType().equals(List.class)) {
+                try {
+                    return (List) method.invoke(object);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
     }
 }
