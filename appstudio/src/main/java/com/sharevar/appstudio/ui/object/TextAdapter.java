bc8d1709663eb96611d3e8f2b898823a117bb3c3
jianglei <260940356@qq.com>
2018-09-03 07:18:47 +0800

    begin runtime from mac

---
+++ appstudio/src/main/java/com/sharevar/appstudio/ui/object/TextAdapter.java
@@ -0,0 +1,23 @@
+package com.sharevar.appstudio.ui.object;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.sharevar.appstudio.object.function.Parameter;
+
+public class TextAdapter extends ParameterAdapter {
+    @Override
+    public View getView() {
+        return null;
+    }
+
+    @Override
+    public Parameter getParameter() {
+        return null;
+    }
+
+    @Override
+    public TextView nameText() {
+        return null;
+    }
+}
