5e692f57f6d585e61c5a2e8bcfd5a67bc7b58855
jianglei <260940356@qq.com>
2018-09-28 18:35:11 +0800

    commit with mac

---
+++ appstudio/src/main/java/com/sharevar/appstudio/runtime/core/StatementLoader.java
@@ -0,0 +1,27 @@
+package com.sharevar.appstudio.runtime.core;
+
+import com.sharevar.appstudio.runtime.core.statement.Statement;
+
+import java.util.List;
+
+public class StatementLoader {
+    private List<Statement> statements;
+
+
+
+    public void insert(Statement statement,int position){
+
+    }
+
+    public void delete(Statement statement){
+
+    }
+
+    public void add(Statement statement){
+
+    }
+
+    public void move(Statement statement,int src,int target){
+
+    }
+}
